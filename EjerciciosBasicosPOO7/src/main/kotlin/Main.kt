ab2420d52bce5e1e9ba185cd30c920fcca039c5b
ManuelAmayaOrozco <127980333+ManuelAmayaOrozco@users.noreply.github.com>
2024-01-29 19:28:44 +0100

    Inicialización Ejercicio Básico POO 7
    Inicialización del ejercicio 7, vacío, solo con un comentario de la clase del 29 de enero para ayudarme para cuando comience a crearlo.

---
+++ EjerciciosBasicosPOO7/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    /*usa private set en el saldo, arrayOfNulls para persona, companion object con constante total cuentas
+    usa .filter para encontrar las cuentas*/
+}
